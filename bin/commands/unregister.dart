6d36610a900e556e3c52f8b1141a7e69cfcaf62c
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-05-15 00:21:48 +0530

    bug fix

--- bin/commands/unregister.dart
+++ bin/commands/unregister.dart
@@ -9,20 +9,22 @@ Future<Message> unregister(Message message, TeleDart teleDart) async {
 
   bool registered = true;
 
-  List team ,solo;
+  List team, solo;
 
   //Adding user name & time of registration
   userName = message.from.username;
 
   //getting the json File
-  io.File teamFile = io.File(join(dirname(io.Platform.script.toFilePath()), 'data', 'registeredTeam.json'));
-  io.File solofile = io.File(join(dirname(io.Platform.script.toFilePath()), 'data', 'registeredSolo.json'));
+  io.File teamFile = io.File(join(
+      dirname(io.Platform.script.toFilePath()), 'data', 'registeredTeam.json'));
+  io.File solofile = io.File(join(
+      dirname(io.Platform.script.toFilePath()), 'data', 'registeredSolo.json'));
 
   try {
     //Decode the jsonFile to List
     team = jsonDecode(teamFile.readAsStringSync());
     solo = jsonDecode(solofile.readAsStringSync());
-    
+
     //Checking if the user Already exists
     team.forEach((element) {
       if (element['userName'] == userName) {
@@ -32,23 +34,26 @@ Future<Message> unregister(Message message, TeleDart teleDart) async {
       }
     });
 
-    if(registered){
-
-    solo.forEach((element) {
-      if (element['userName'] == userName) {
-        registered = false;
-        team.remove(element);
-        return;
-      }
-     });
+    if (registered) {
+      solo.forEach(
+        (element) {
+          if (element['userName'] == userName) {
+            registered = false;
+            solo.remove(element);
+            return;
+          }
+        },
+      );
     }
     if (!registered) {
-      return teleDart.replyMessage(message, 'Hey @${message.from.username} you were not registered :|');
+      return teleDart.replyMessage(
+          message, 'Hey @${message.from.username} you were not registered :|');
     }
 
     //encoding back the user to the file
     await io.File(teamFile.path).writeAsStringSync(jsonEncode(team));
-    return teleDart.replyMessage(message, '@${message.from.username} you have been un-registered from this weeks challenge.');
+    return teleDart.replyMessage(message,
+        '@${message.from.username} you have been un-registered from this weeks challenge.');
   } catch (e) {
     print(e.toString());
     return teleDart.replyMessage(message, 'Oops Something went Wrong!');

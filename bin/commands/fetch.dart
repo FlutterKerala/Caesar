24a33295eb118b88d89981507a451b28be37c602
icodelifee <mithunsmenon@gmail.com>
2020-05-15 23:59:03 +0530

    Add catch block in fetch

--- bin/commands/fetch.dart
+++ bin/commands/fetch.dart
@@ -26,46 +26,50 @@ Future<Message> fetch(Message message, TeleDart teleDart) async {
     //Decode the jsonFile to List
     team = jsonDecode(teamFile.readAsStringSync());
     solo = jsonDecode(solofile.readAsStringSync());
-    String teamRes = '', soloRes = '';
-    int index = 1;
-
-    teamRes += 'Members Registered For Team\n\n';
-    soloRes += 'Members Registered As Solo\n\n';
-
-    for (Map element in team) {
-      String uName;
-      if (isNumeric(element['userName'])) {
-        ChatMember userInfo = await teleDart.telegram
-            .getChatMember(-1001256945986, int.parse(element['userName']));
-        uName =
-            '<a href="tg://user?id=${element['userName']}">${userInfo.user.first_name + userInfo.user.last_name ?? ""}</a>';
-      } else {
-        uName = '@${element['userName']}';
-      }
-      teamRes +=
-          "${index++}. $uName with ${element['expirience']} month(s) experience 🧑‍💻\n";
-    }
-    index = 1;
-    for (Map element in solo) {
-      String uName;
-      if (isNumeric(element['userName'])) {
-        ChatMember userInfo = await teleDart.telegram
-            .getChatMember(-1001256945986, int.parse(element['userName']));
-        uName =
-            '<a href="tg://user?id=${element['userName']}">${userInfo.user.first_name + userInfo.user.last_name ?? ""}</a>';
-      } else {
-        uName = '@${element['userName']}';
-      }
-      soloRes +=
-          "${index++}. $uName with ${element['expirience']} month(s) experience 🧑‍💻\n";
-    }
-
-    await teleDart.replyMessage(message, soloRes,
-        parse_mode: 'HTML', withQuote: true, disable_web_page_preview: true);
-    return teleDart.replyMessage(message, teamRes,
-        parse_mode: 'HTML', withQuote: true, disable_web_page_preview: true);
   } catch (e) {
     print(e.toString());
     return teleDart.replyMessage(message, 'Oops Something went wrong!');
   }
+  String teamRes = '', soloRes = '';
+  int index = 1;
+
+  teamRes += 'Members Registered For Team\n\n';
+  soloRes += 'Members Registered As Solo\n\n';
+
+  for (Map element in team) {
+    String uName;
+    if (isNumeric(element['userName'])) {
+      ChatMember userInfo = await teleDart.telegram
+          .getChatMember(-1001256945986, int.parse(element['userName']));
+      uName =
+          '<a href="tg://user?id=${element['userName']}">${userInfo.user.first_name + userInfo.user.last_name ?? ""}</a>';
+    } else {
+      uName = '@${element['userName']}';
+    }
+    teamRes +=
+        "${index++}. $uName with ${element['expirience']} month(s) experience 🧑‍💻\n";
+  }
+  index = 1;
+  for (Map element in solo) {
+    String uName;
+    if (isNumeric(element['userName'])) {
+      ChatMember userInfo = await teleDart.telegram
+          .getChatMember(-1001256945986, int.parse(element['userName']));
+      uName =
+          '<a href="tg://user?id=${element['userName']}">${userInfo.user.first_name + userInfo.user.last_name ?? ""}</a>';
+    } else {
+      uName = '@${element['userName']}';
+    }
+    soloRes +=
+        "${index++}. $uName with ${element['expirience']} month(s) experience 🧑‍💻\n";
+  }
+
+  await teleDart
+      .replyMessage(message, soloRes,
+          parse_mode: 'HTML', withQuote: true, disable_web_page_preview: true)
+      .catchError((onError) => print(onError));
+  return teleDart
+      .replyMessage(message, teamRes,
+          parse_mode: 'HTML', withQuote: true, disable_web_page_preview: true)
+      .catchError((onError) => print(onError));
 }

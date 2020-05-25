718c16fa735868a5926a77748ff786e00556ed16
Prathyush <prathyushpramod@gmail.com>
2020-05-26 01:03:04 +0530

    Make it rose water

--- bin/commands/aana.dart
+++ bin/commands/aana.dart
@@ -3,6 +3,6 @@ import 'package:teledart/teledart.dart';
 
 Future<Message> aana(Message message, TeleDart teleDart) {
   return teleDart.replyMessage(message,
-      '🐘💦',
+      '🐘🌹💦',
       parse_mode: 'html');
 }
